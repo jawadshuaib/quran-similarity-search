eb5a03b04908ee4505ee4145c7c49645ee2d2895
Jawad <jawad.php@gmail.com>
2022-09-01 18:44:15 -0400

    Added a background

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,7 +12,7 @@
 	class="bg-cover lg:h-80 md:h-60 sm:h-28 h-10 w-full shadow-lg hue-rotate-30"
 	style="background-image: url('pattern-alhambra.jpeg')"
 />
-<div class="p-8 max-w-6xl mx-auto">
+<div class="p-8 max-w-6xl mx-auto bg-white">
 	<Nav />
 	<Transition url={$page.url}>
 		<slot />
