b1501971b48f6ef5335cda5138da8f1b5e509478
Jawad <jawad.php@gmail.com>
2022-09-04 00:31:28 -0400

    Updated about

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -25,8 +25,9 @@
 			I used a machine learning model known an Word2Vec to find similar verses. A cosine similarity
 			algorithm determed the distance between each verse. This permutation resulted in many million
 			rows of data, which was then stemmed for reducing dimensionality. The final dataset is
-			readable through an API. The web application accesses this API using a front-end built in
-			Svelte JS and Tailwind CSS.
+			readable through an API. A cache system was added to enhance load times and reduce expensive
+			calls to the database. The web application accesses this API using a front-end built in Svelte
+			JS and Tailwind CSS.
 		</p>
 		<p>
 			Front-end code is available on <a
