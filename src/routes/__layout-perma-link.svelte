f1efc0f41e9c43921dadf53bae7b195503073021
Jawad <jawad.php@gmail.com>
2022-09-08 23:59:46 -0400

    h-full

--- src/routes/__layout-perma-link.svelte
+++ src/routes/__layout-perma-link.svelte
@@ -11,7 +11,10 @@
 	class="bg-cover lg:h-80 md:h-60 sm:h-28 h-10 w-full shadow-lg"
 	style="background-image: url('../pattern-mosque.jpeg')"
 />
-<div style="background-image: url('../bg-white-wall.webp');  background-repeat: repeat-y repeat-x;">
+<div
+	class="h-full"
+	style="background-image: url('../bg-white-wall.webp');  background-repeat: repeat-y repeat-x;"
+>
 	<div class="p-8 max-w-6xl mx-auto bg-white">
 		<Nav />
 		<Transition url={$page.url}>
