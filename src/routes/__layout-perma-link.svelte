0e49d0bc18240bacaed9d42776ace94fef6d7edd
Jawad <jawad.php@gmail.com>
2022-09-09 00:20:16 -0400

    Added abs path for bg

--- src/routes/__layout-perma-link.svelte
+++ src/routes/__layout-perma-link.svelte
@@ -11,16 +11,12 @@
 	class="bg-cover lg:h-80 md:h-60 sm:h-28 h-10 w-full shadow-lg"
 	style="background-image: url('../pattern-mosque.jpeg')"
 />
-<div
-	class="h-full"
-	style="background-image: url('../bg-white-wall.webp');  background-repeat: repeat-y repeat-x;"
->
-	<div class="p-8 max-w-6xl mx-auto bg-white">
-		<Nav />
-		<Transition url={$page.url}>
-			<slot />
-		</Transition>
-	</div>
+
+<div class="p-8 max-w-6xl mx-auto bg-white">
+	<Nav />
+	<Transition url={$page.url}>
+		<slot />
+	</Transition>
 </div>
 
 <style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -13,19 +13,14 @@
 	class="bg-cover lg:h-80 md:h-60 sm:h-28 h-10 w-full shadow-lg hue-rotate-30"
 	style="background-image: url('pattern-alhambra.jpeg')"
 />
-<div
-	class="h-full"
-	style="background-image: url('bg-white-wall.webp');  background-repeat: repeat-y repeat-x;"
->
-	<div class="p-8 max-w-6xl mx-auto bg-white">
-		<Nav />
-		<Transition url={$page.url}>
-			<slot />
-		</Transition>
-	</div>
-	<div class="p-2 max-w-6xl mx-auto text-center">
-		<SettingsBar />
-	</div>
+<div class="p-8 max-w-6xl mx-auto bg-white">
+	<Nav />
+	<Transition url={$page.url}>
+		<slot />
+	</Transition>
+</div>
+<div class="p-2 max-w-6xl mx-auto text-center">
+	<SettingsBar />
 </div>
 
 <style>
