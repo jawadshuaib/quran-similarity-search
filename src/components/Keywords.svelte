1ff0d7726e32bd3687360b42a070aab8cb10fda6
Jawad <jawad.php@gmail.com>
2022-09-10 20:12:52 -0400

    Keyword search added to homepage

--- src/components/Keywords.svelte
+++ src/components/Keywords.svelte
@@ -1,12 +1,16 @@
 <script>
 	// Internal
 	import { goto } from '$app/navigation';
+	// API
+	import keywordsSearch from '../api/fetch-keywords-search';
 
 	export let verse;
 
 	let canSearch = false;
 	let keywords = [];
 	let el;
+	let totalResults; // Total results for keywords selected
+	let totalKeywordsSelected; // Total keywords selected (by clicking on their div)
 
 	// Execute the following whenever verse changes
 	$: verse,
@@ -15,9 +19,36 @@
 				keywords = verse.arabic_lemmatized.split(' ');
 				keywords = keywords.reverse();
 			}
+
+			canSearch = false;
+			totalResults = 0;
+			totalKeywordsSelected = 0;
+
+			if (el !== undefined) {
+				// Strangely enough, I have to do this twice for it to de-select all selected keywords
+				deselectAll();
+				deselectAll();
+			}
 		})();
 
-	// Select the keywords
+	// Find out how many ayas contain the keywords
+	// This is displayed to the user as they select keywords
+	function totalResultsForKeywords() {
+		const keywords = getKeywordsSelected();
+		totalKeywordsSelected = keywords.split(',').length;
+
+		if (totalKeywordsSelected > 0) {
+			keywordsSearch(keywords).then((json) => {
+				totalResults = 0;
+				if (json.results !== undefined && json.results.length > 0) {
+					totalResults = json.results.length;
+				}
+			});
+		}
+	}
+
+	// Select the divs that contain the keywords
+	// We add the 'selected' class to keep track of keywords are selected
 	function select(e) {
 		if (e.target.classList.contains('keyword')) {
 			const selected = 'selected';
@@ -35,6 +66,14 @@
 
 	// Redirect the user to the keywords search page
 	function search() {
+		const keywords = getKeywordsSelected();
+		// Search for selected keywords
+		goto(`/keywords/${keywords}`);
+	}
+
+	// Get the keywords selected by the user by looking up the 'selected' class
+	// in each div that contains a keyword
+	function getKeywordsSelected() {
 		// Get keywords selected for search
 		const els = el.getElementsByClassName('selected');
 		let selected = '';
@@ -43,22 +82,31 @@
 		});
 
 		// Remove trailing comma
-		selected = selected.replace(/(,$)/g, '');
+		return selected.replace(/(,$)/g, '');
+	}
 
-		// Search for selected keywords
-		goto(`/keywords/${selected}`);
+	function deselectAll() {
+		const els = el.getElementsByClassName('selected');
+
+		[].forEach.call(els, function (el) {
+			el.classList.remove('border-orange-400');
+			el.classList.remove('bg-orange-100');
+			el.classList.remove('selected');
+		});
 	}
 </script>
 
 <div class="border-2 border-slate-50 rounded p-5 items-center">
+	<!-- Div's containing keywords and the search button -->
 	<div
 		on:click={select}
-		class="flex flex-wrap justify-center my-2 text-2xl px-5 font-cormorant text-slate-400 text-center"
+		on:click={totalResultsForKeywords}
+		class="flex flex-wrap justify-center mt-2 text-2xl px-5 font-cormorant text-slate-400 text-center"
 		bind:this={el}
 	>
 		{#each keywords as keyword}
 			<div
-				class="keyword rounded flex-initial w-24 border-2 m-2 py-2 hover:border-orange-400 hover:border-2 hover:cursor-pointer"
+				class="keyword rounded flex-initial w-24 border-2 mx-2 mt-2 py-2 hover:border-orange-400 hover:border-2 hover:cursor-pointer"
 			>
 				{keyword}
 			</div>
@@ -67,10 +115,25 @@
 		{#if canSearch}
 			<div
 				on:click={search}
-				class="wrap-center rounded flex-initial m-2 h-14 bg-orange-500 text-white px-3  hover:bg-orange-700 hover:cursor-pointer"
+				class="wrap-center rounded flex-initial mx-2 mt-2.5 h-12 bg-orange-500 text-white px-3  hover:bg-orange-700 hover:cursor-pointer"
 			>
 				Search Keywords
 			</div>
+		{/if}
+	</div>
+
+	<!-- Show user pre-emptively how many results the combination of keywords selected will show -->
+	<div class="mt-2">
+		{#if totalResults > 0}
+			<div class="text-slate-400 text-center">
+				{#if totalKeywordsSelected > 1}
+					Found <span class="font-medium">{totalResults} results</span> for this combination of keywords
+				{:else}
+					Found <span class="font-medium">{totalResults} results</span> for this keyword.
+				{/if}
+			</div>
+		{:else}
+			<div>&nbsp;</div>
 		{/if}
 	</div>
 </div>
