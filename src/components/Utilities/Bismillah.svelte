0372d3c0efb45c71e96a624bb73e2ae127de1819
Jawad <jawad.php@gmail.com>
2022-09-11 21:47:18 -0400

    Updated module to reflect different script for 786

--- src/components/Utilities/Bismillah.svelte
+++ src/components/Utilities/Bismillah.svelte
@@ -6,7 +6,7 @@
 	<div class="bismilla">
 		<div class="bismilla-text text-3xl text-slate-400 text-center my-2">
 			<span title="In the name of God, the Gracious, the Merciful."
-				>بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ</span
+				>بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ</span
 			>
 		</div>
 	</div>
