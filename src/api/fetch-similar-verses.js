dca8fac37271581272304f36e0c5712586f28399
Jawad <jawad.php@gmail.com>
2022-09-28 17:02:14 -0400

    Fixed json address

--- src/api/fetch-similar-verses.js
+++ src/api/fetch-similar-verses.js
@@ -36,7 +36,7 @@ const baseURL = `${envVariables.isDeveloperMode ? envVariables.isDeveloperEndPoi
 
 const similarVerses = async (options) => { 
 
-	const url = options.surahNumber == 1 && options.ayaNumber == 1 ? `src/json/1:1.json` : `${baseURL}/similar.php?translation=${options.translationId}&surah_number=${options.surahNumber}&aya_number=${options.ayaNumber}&method=${options.method}`;    	
+	const url = options.surahNumber == 1 && options.ayaNumber == 1 ? `/json/1:1.json` : `${baseURL}/similar.php?translation=${options.translationId}&surah_number=${options.surahNumber}&aya_number=${options.ayaNumber}&method=${options.method}`;    	
 
   try {
     const response = await fetch(url);
