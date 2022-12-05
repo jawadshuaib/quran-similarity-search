e519e3a38ff56376e32d520c19a3991a130f3477
Jawad <jawad.php@gmail.com>
2022-12-04 23:41:27 -0500

    Removed proxy

--- src/scripts/env-variables.js
+++ src/scripts/env-variables.js
@@ -1,6 +1,7 @@
 const envVariables = {  
   isDeveloperMode: false,
-  corsAnywhereProxy: 'https://glacial-brushlands-90091.herokuapp.com/',
+  corsAnywhereProxy: '',
+  //corsAnywhereProxy: 'https://glacial-brushlands-90091.herokuapp.com/',
   isDeveloperEndPoint: 'http://localhost/quran/api',
   isProductionEndPoint: 'http://18.220.86.213/api',
   imageProxy: 'https://quran-ml.netlify.app'
