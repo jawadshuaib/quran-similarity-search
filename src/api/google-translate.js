f155bb7898deaa324ac7bc9374888df4f2cb0a22
Jawad <jawad.php@gmail.com>
2023-10-24 15:11:05 -0400

    Removed clg

--- src/api/google-translate.js
+++ src/api/google-translate.js
@@ -1,24 +1,26 @@
 import { envVariables } from '../scripts/env-variables';
 
-const baseURL = `${envVariables.isDeveloperMode ? envVariables.isDeveloperEndPoint : envVariables.corsAnywhereProxy + envVariables.isProductionEndPoint}`;
+const baseURL = `${
+	envVariables.isDeveloperMode
+		? envVariables.isDeveloperEndPoint
+		: envVariables.corsAnywhereProxy + envVariables.isProductionEndPoint
+}`;
 
-const translate = async (text) => {   
-    
-  const url = `${baseURL}/google-translate.php?text=${text.trim()}`;
-  console.log(url);
-  try {
-    const response = await fetch(url);
-    const json = await response.json();
-    
-    if (json.error !== undefined) {
-      throw new Error(json.error.message);
-    }
-    console.log(json);  
-    return json.translated;
-  }
-  catch (error) {  
-    return { error }
-  }
-}
+const translate = async (text) => {
+	const url = `${baseURL}/google-translate.php?text=${text.trim()}`;
+
+	try {
+		const response = await fetch(url);
+		const json = await response.json();
+
+		if (json.error !== undefined) {
+			throw new Error(json.error.message);
+		}
+
+		return json.translated;
+	} catch (error) {
+		return { error };
+	}
+};
 
 export default translate;
